dbead642754abe4a1bf6acbaefb91786eb153d00
GodwinJoe06 <godwinjoe2005@gmail.com>
2025-09-17 11:08:07 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@ export const LANGUAGE_VERSIONS = {
 
 export const CODE_SNIPPETS = {
   javascript: `\nfunction greet(msg) {\n\tconsole.log(msg);\n}\n\ngreet("Vanakam");\n`,
-  typescript: `\ntype Params = {\n\tmsg: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log(data.msg);\n}\n\ngreet({ name: "Vanakam" });\n`,
+  typescript: `\ntype Params = {\n\tmsg: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log(data.msg);\n}\n\ngreet({ msg: "Vanakam" });\n`,
   python: `\ndef greet(msg):\n\tprint(msg)\n\ngreet("Vanakam")\n`,
   java: `\npublic class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Vanakam");\n\t}\n}\n`,
   csharp:
